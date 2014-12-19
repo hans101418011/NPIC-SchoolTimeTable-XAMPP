a4f187ceb76a09e19a545edac74468bd83e5d7f1
Hans <hans5300609@gmail.com>
2014-12-20 07:03:08 +0800

    new

---
+++ interface/_member.php
@@ -0,0 +1,63 @@
+<?php
+if($_GET['logout']==1)
+{
+	destroy_session_and_data();
+	unset($user_id);
+}
+$user_id = "";
+if($_POST['login']=='1')
+{
+	$user_id = $_POST['user_id'];
+	$user_pw = $_POST['user_pw'];
+	$sql = "SELECT * FROM `member` WHERE `email` LIKE '".$user_id."' LIMIT 0,1";
+	$result = mysql_query($sql,$link);
+	$memberdata=mysql_fetch_assoc($result);
+	if($memberdata)
+	{
+		if(md5($user_pw)==$memberdata['password'])
+		{
+			$_SESSION['login'] = 'success';
+			$_SESSION['user_name'] = $memberdata['name'];
+		}
+		else
+			echo "login error ";
+	}
+	else
+		echo "login error ";
+}
+
+if($_SESSION['login']=='success')
+{
+	echo $memberdata["name"]."你好";
+?>
+	<a href="./index.php" class="bt">查詢</a>
+	<a href="./user.php" class="bt">模擬選課</a>
+	<a href="./setup.php" class="bt">設定</a>
+	<a href="./index.php?logout=1" class="bt">登出</a>
+<?php
+}
+else
+{
+?>
+	<form name="login" method="post" action="index.php">
+		<input type="text" name="user_id" value="<?=$user_id?>" placeholder="User ID">
+		<input type="password" name="user_pw" placeholder="Password">
+		<input type="hidden" name="login" value="1">
+		<input type="submit" value="登入" class="bt">
+	</form>
+	<a href="./register.php" class="bt">註冊</a>
+<?php
+}	
+?>
+
+<?php
+
+function destroy_session_and_data()
+{
+	session_start();
+	$_SESSION=array();
+	if(session_id()!= "" || isset($_COOKIE[session_name()]))
+		setcookie(session_name(),'',time()-2592000,'/');
+	session_destroy();
+}
+?>
---
+++ interface/register.php
@@ -0,0 +1,5 @@
+<?php
+
+$sql="INSERT INTO `npic_subject`.`member` (`id`, `name`, `email`, `password`) VALUES (NULL, 'Hans', 's101418011@stmail.nptu.edu.tw', MD5('chen')");
+
+?>
